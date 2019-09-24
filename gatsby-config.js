4546687e8d974264260315cebbf815c694d472c7
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-09-23 17:03:51 -0700

    radiology interview app

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "Location",
+        fieldName: "interviews",
+        url:
+          "https://us-central1-eventstack-prod.cloudfunctions.net/radiology-data",
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,60 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import "semantic-ui-css/semantic.min.css"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+import { Icon, Label, Menu, Table } from "semantic-ui-react"
+
+const TableExamplePagination = ({ rows }) => (
+  <Table celled>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Name</Table.HeaderCell>
+        <Table.HeaderCell>Location</Table.HeaderCell>
+        <Table.HeaderCell>Sent Date</Table.HeaderCell>
+        <Table.HeaderCell>Dates</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+      {rows.map(({ name, location, interviewSentDate, interviewSlots }) => {
+        return (
+          <Table.Row>
+            <Table.Cell>{name}</Table.Cell>
+
+            <Table.Cell width={4}>{location}</Table.Cell>
+
+            <Table.Cell>{interviewSentDate}</Table.Cell>
+
+            <Table.Cell>{interviewSlots}</Table.Cell>
+          </Table.Row>
+        )
+      })}
+    </Table.Body>
+  </Table>
 )
 
-export default IndexPage
+const SecondPage = ({ data }) => {
+  console.log(data)
+  const rows = data.interviews.interviewsForApplication
+  return (
+    <section style={{ maxWidth: 1024, margin: "64px auto" }}>
+      <TableExamplePagination rows={rows} />
+    </section>
+  )
+}
+
+export const interviews = graphql`
+  {
+    interviews {
+      interviewsForApplication {
+        location
+        name
+        interviewSentDate
+        interviewSlots
+      }
+    }
+  }
+`
+
+export default SecondPage
